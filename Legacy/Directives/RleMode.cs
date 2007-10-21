2b937866fd4cd1efffad4d605c71b846e40c7750
Ben Ryves <benryves@benryves.com>
2007-10-21 11:56:03 +0000

    Addition of .rlemode legacy directive.

---
+++ Legacy/Directives/RleMode.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Brass3.Attributes;
+using Brass3.Plugins;
+
+using System.ComponentModel;
+using Brass3;
+
+namespace Legacy.Directives {
+	[Syntax(".rlemode run_indicator [, value_first]")]
+	[Description("Sets the current RLE mode.")]
+	[Remarks("Sets the current RLE mode - first, the byte value used to represent a run (defaults to $91), followed by a flag to set whether the value or the length is written first after the run indicator (defaults to true).")]
+	[Category("Data")]
+	[Warning("This legacy directive only affects the behaviour of the <c>.incbmp</c> plugin.")]
+	[SeeAlso(typeof(IncBmp))]
+	public class RleMode : IDirective {
+
+		public string[] Names { get { return new string[] { "rlemode" }; } }
+		public string Name { get { return this.Names[0]; } }
+
+		public void Invoke(Compiler compiler, TokenisedSource source, int index, string directive) {
+
+			IncBmp Parent = compiler.GetPluginInstanceFromType<IncBmp>();
+			if (Parent == null) throw new CompilerExpection(source.Tokens[index], ".incbmp plugin not loaded.");
+
+			object[] o = source.GetCommaDelimitedArguments(compiler, index + 1, new TokenisedSource.ArgumentType[] { 
+				TokenisedSource.ArgumentType.Value,
+				TokenisedSource.ArgumentType.Value |  TokenisedSource.ArgumentType.Optional
+			});
+
+			Parent.RLE_Flag = (byte)(double)o[0];
+			if (o.Length > 1) Parent.RLE_ValueFirst = (double)o[1] != 0d;
+		}
+
+		
+	}
+}
