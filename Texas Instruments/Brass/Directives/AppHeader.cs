803c0e2da4cacb023d236f08255e9c0d370a163f
Ben Ryves <benryves@benryves.com>
2007-11-14 22:55:07 +0000

    Tentative .appheader directive (source files only).

---
+++ Texas Instruments/Brass/Directives/AppHeader.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Brass3;
+using Brass3.Plugins;
+using Brass3.Attributes;
+namespace TexasInstruments.Brass.Directives {
+	/*public class AppHeader : IDirective {
+
+	}*/
+}
