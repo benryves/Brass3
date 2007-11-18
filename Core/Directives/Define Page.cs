de6f97e691cc66e3d132162167167f2b6c80ddac
Ben Ryves <benryves@benryves.com>
2007-11-18 22:09:15 +0000

    .page/.defpage have improved error detection and handling for repeated use and use of .page before .defpage.

--- Core/Directives/Define Page.cs
+++ Core/Directives/Define Page.cs
@@ -10,14 +10,14 @@ namespace Core.Directives {
 
 	[Syntax(".defpage page, size, address")]
 	[Description("Defines the base address and size of a page.")]
-	[Remarks("If you use this directive before <c>.page</c> the program counter and output counter values are set for you automatically.")]
+	[Remarks("If you use this directive before using the <see cref=\"page\" /> directive the program counter and output counter values are set for you automatically.")]
 	[Category("Output Manipulation")]
 	[SeeAlso(typeof(Output.RawPages))]
 	[SeeAlso(typeof(Directives.Page))]
 	public class DefPage : IDirective {
 
 		public void Invoke(Compiler compiler, TokenisedSource source, int index, string directive) {
-			if (compiler.CurrentPass != AssemblyPass.CreatingLabels) return;
+
 			Output.RawPages PagedRawWriter;
 			if ((PagedRawWriter = compiler.GetPluginInstanceFromType(typeof(Output.RawPages)) as Output.RawPages) == null) throw new CompilerExpection(source, "Paged raw writer plugin not loaded.");
 
@@ -29,10 +29,29 @@ namespace Core.Directives {
 				return (int)(double)arg;
 			});
 
-			if (PagedRawWriter.PageDeclarations.ContainsKey(Args[0])) PagedRawWriter.PageDeclarations.Remove(Args[0]);
-			PagedRawWriter.PageDeclarations.Add(Args[0], new Core.Output.RawPages.PageDeclaration(Args[1], Args[2]));
+			switch (compiler.CurrentPass) {
+				case AssemblyPass.CreatingLabels: {
 
+						if (PagedRawWriter.PageDeclarations.ContainsKey(Args[0])) PagedRawWriter.PageDeclarations.Remove(Args[0]);
+						PagedRawWriter.PageDeclarations.Add(Args[0], new Core.Output.RawPages.PageDeclaration(Args[1], Args[2]));
+
+					} break;
+				case AssemblyPass.WritingOutput: {
+
+						if (compiler.GetOutputDataOnPage(Args[0]).Length > 0) {
+							compiler.OnWarningRaised(string.Format("Data already output on page {0}.", Args[0]));
+						} else {
+							Page P;
+							if ((P = compiler.GetPluginInstanceFromType<Page>()) != null) {
+								if (P.SwitchedToPages.Contains(Args[0])) {
+									compiler.OnWarningRaised(string.Format("Page directive used before page {0} was defined.", Args[0]));
+								}
+							}
+						}
+
+					} break;
+
+			}
 		}
-
 	}
 }
--- Core/Directives/Page.cs
+++ Core/Directives/Page.cs
@@ -11,21 +11,66 @@ namespace Core.Directives {
 	[Syntax(".page <param>number</param>")]
 	[Description("Sets the current page number.")]
 	[CodeExample(".page 2\r\n.echoln \"Current page = \", :$")]
+	[Remarks(@"This directive sets the page number of the program counter and output counter.
+The first time a particular page is switched to using this directive it checks to see if the page has been defined (with <see cref=""defpage""/>) and sets the program counter and output counter accordingly (otherwise it simply resets both to zero).
+Subsequent use of this directive only changes the page numbers and not the program counter or output counter.")]
 	[Category("Output Manipulation")]
+	[SeeAlso(typeof(DefPage))]
+
 	public class Page : IDirective {
 
-		public void Invoke(Compiler compiler, TokenisedSource source, int index, string directive) {
-			int[] Args = source.GetCommaDelimitedArguments(index + 1, 1);
-			compiler.Labels.ProgramCounter.Page = (int)source.EvaluateExpression(compiler, Args[0]).NumericValue;
+		Queue<int> PageOffsetAddresses;
+		internal List<int> SwitchedToPages;
 
-			Output.RawPages PagedRawWriter;
-			if ((PagedRawWriter = compiler.GetPluginInstanceFromType(typeof(Output.RawPages)) as Output.RawPages) != null) {
-				Output.RawPages.PageDeclaration PD;
-				if (PagedRawWriter.PageDeclarations.TryGetValue(compiler.Labels.ProgramCounter.Page, out PD)) {
-					compiler.Labels.ProgramCounter.NumericValue = PD.Address;
-					compiler.Labels.OutputCounter.NumericValue = PD.Address;
+		public Page(Compiler compiler) {
+			
+			this.PageOffsetAddresses = new Queue<int>();
+			this.SwitchedToPages = new List<int>();
+
+			compiler.PassBegun += delegate(object sender, EventArgs e) {
+				if (compiler.CurrentPass == AssemblyPass.CreatingLabels) {
+					this.PageOffsetAddresses.Clear();
+					this.SwitchedToPages.Clear();
 				}
+			};
+		}
+
+		public void Invoke(Compiler compiler, TokenisedSource source, int index, string directive) {
+
+			int Argument = (int)(double)source.GetCommaDelimitedArguments(compiler, index + 1, TokenisedSource.ValueArgument)[0];
+			
+			compiler.Labels.ProgramCounter.Page = Argument;
+			compiler.Labels.OutputCounter.Page = Argument;
+
+			switch (compiler.CurrentPass) {
+				case AssemblyPass.CreatingLabels:
+
+					if (!this.SwitchedToPages.Contains(compiler.Labels.ProgramCounter.Page)) {
+						int DefaultAddress = 0;
+						Output.RawPages PagedRawWriter;
+						if ((PagedRawWriter = compiler.GetPluginInstanceFromType(typeof(Output.RawPages)) as Output.RawPages) != null) {
+							Output.RawPages.PageDeclaration PD;
+							if (PagedRawWriter.PageDeclarations.TryGetValue(compiler.Labels.ProgramCounter.Page, out PD)) {
+								DefaultAddress = PD.Address;
+							}
+						}
+						compiler.Labels.ProgramCounter.NumericValue = DefaultAddress;
+						compiler.Labels.OutputCounter.NumericValue = DefaultAddress;
+						this.SwitchedToPages.Add(compiler.Labels.ProgramCounter.Page);
+					}
+
+					PageOffsetAddresses.Enqueue((int)compiler.Labels.ProgramCounter.NumericValue);
+					PageOffsetAddresses.Enqueue((int)compiler.Labels.OutputCounter.NumericValue);
+
+					break;
+				case AssemblyPass.WritingOutput:
+					compiler.Labels.ProgramCounter.NumericValue = PageOffsetAddresses.Dequeue();
+					compiler.Labels.OutputCounter.NumericValue = PageOffsetAddresses.Dequeue();
+					break;
 			}
+
+			
+			
 
 		}
 
