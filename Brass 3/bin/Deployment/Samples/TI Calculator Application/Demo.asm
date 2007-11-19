0ea087c24d7423ad41a34cf448cb535d41affb6e
Ben Ryves <benryves@benryves.com>
2007-11-19 00:24:09 +0000

    Expanded sample program to two pages.

--- Brass 3/bin/Deployment/Samples/TI Calculator Application/Demo.asm
+++ Brass 3/bin/Deployment/Samples/TI Calculator Application/Demo.asm
@@ -1,5 +1,14 @@
 ﻿.page 0
 
+	; Jump to the main entry point.
+	jp Main
+	
+	; Insert the branch table.
+	.branch OffPageCall
+
+
+Main
+
 	; Clear the screen.
 	.bcall _clrLCDFull
 
@@ -17,12 +26,36 @@
 	ld hl,OP1
 	.bcall _putS
 	
+	; Move down a line.
+	.bcall _newLine
+	
+	; Call a function on page 1 (note the _underscore).
+	.bcall _OffPageCall
+	
+	.echoln strformat("{0:X4}", _OffPageCall)
+	
 	; Wait for a key.
 	.bcall _getKey
 
 	; Exit the application
 	.bjump _JForceCmdNoChar
 
-StringPage0
-	.byte "Brass 3 Demo", 0
-StringPage0Length = $-StringPage0
+	StringPage0 .byte "Brass 3 Demo.", 0 \ StringPage0Length = $-StringPage0
+
+.page 1
+
+OffPageCall
+
+	; Copy string from ROM to RAM for displaying.
+	ld hl,StringPage1
+	ld de,OP1
+	ld bc,StringPage1Length
+	ldir
+	
+	; Display the string.
+	ld hl,OP1
+	.bcall _putS
+	
+	ret
+
+	StringPage1 .byte "Press any key.", 0 \ StringPage1Length = $-StringPage1
