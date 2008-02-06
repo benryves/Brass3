b452450871ab1f7cb632f047ecde1f519414674a
Ben Ryves <benryves@benryves.com>
2008-02-06 01:13:35 +0000

    Addition of rudimentary debugging functionality (via plugins, of course).

---
+++ Brass 3/Brass/Plugins/IDebugger.cs
@@ -0,0 +1,16 @@
+﻿namespace BeeDevelopment.Brass3.Plugins {
+	
+	/// <summary>
+	/// Defines the interface that debuggers need to implement.
+	/// </summary>
+	public interface IDebugger : IPlugin {
+
+		/// <summary>
+		/// Starts the debugger.
+		/// </summary>
+		/// <param name="compiler">The compiler instance to start the debugger with.</param>
+		/// <param name="debuggingEnabled">True if the debugger is enabled by default, false if we wish to run without debugging.</param>
+		void Start(Compiler compiler, bool debuggingEnabled);
+
+	}
+}
